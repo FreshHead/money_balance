cc85926225f4d4afefa109f85fe3469cd283b9c9
Alexander Kostarev <UniverAlex@ya.ru>
2018-01-27 05:15:05 +0300

    get SQL to work

--- money_balance/controller.py
+++ money_balance/controller.py
@@ -1,43 +1,109 @@
 from money_balance import Gtk
-from money_balance.view import NotebookWindow, OperationEntryWindow
+from money_balance.view import NotebookWindow, OperationEntryWindow, GoalEntryWindow, TypeEntryWindow
 
 
 class Controller:
     def __init__(self, model):
         self.model = model
-        self.tree_view_window = NotebookWindow(model.operation_list_store, model.goal_list_store, model.type_list_store)
+        self.notebook_window = NotebookWindow(model.operation_list_store, model.goal_list_store, model.type_list_store)
         self.entry_window = None
         self.selected_row = None
 
-        self.tree_view_window.connect('operation-insert', self.open_insert_form)
-        self.tree_view_window.connect('goal-insert', self.open_update_form)
-        self.tree_view_window.connect('type-insert', self.delete_selected_row)
-        self.tree_view_window.connect('delete-event', Gtk.main_quit)
-        self.tree_view_window.show_all()
+        self.notebook_window.connect('operation-insert', self.open_insert_operation_form)
+        self.notebook_window.connect('operation-update', self.open_update_operation_form)
+        self.notebook_window.connect('operation-delete', self.delete_selected_operation_row)
 
-    def open_insert_form(self, widget):
+        self.notebook_window.connect('goal-insert', self.open_insert_goal_form)
+        self.notebook_window.connect('goal-update', self.open_update_goal_form)
+        self.notebook_window.connect('goal-delete', self.delete_selected_goal_row)
+
+        self.notebook_window.connect('type-insert', self.open_insert_type_form)
+        self.notebook_window.connect('type-update', self.open_update_type_form)
+        self.notebook_window.connect('type-delete', self.delete_selected_type_row)
+
+        self.notebook_window.connect('delete-event', Gtk.main_quit)
+        self.notebook_window.show_all()
+
+    def open_insert_operation_form(self, widget):
         self.entry_window = OperationEntryWindow()
         self.entry_window.connect('save-inserted', self.insert_in_list_store)
         self.entry_window.show_for_insert()
 
-    def open_update_form(self, widget):
-        model, self.selected_row = self.tree_view_window.operations_view.get_selection().get_selected()
+    def open_update_operation_form(self, widget):
+        model, self.selected_row = self.notebook_window.operation_view.get_selection().get_selected()
         if self.selected_row is not None:
             self.entry_window = OperationEntryWindow()
             self.entry_window.connect('save-updated', self.update_in_list_store)
             self.entry_window.show_for_update(model[self.selected_row])
 
-    def delete_selected_row(self, widget):
-        model, selected_row = self.tree_view_window.operations_view.get_selection().get_selected()
-        self.model.list_store.remove(selected_row)
+    def delete_selected_operation_row(self, widget):
+        (model, pathlist) = self.notebook_window.operation_view.get_selection().get_selected_rows()
+        tree_iter = model.get_iter(pathlist[0])
+        id = model.get_value(tree_iter, 0)
+        self.model.delete_operation(id)
+
+    def open_insert_goal_form(self, widget):
+        self.entry_window = GoalEntryWindow()
+        self.entry_window.connect('save-inserted', self.insert_in_list_store)
+        self.entry_window.show_for_insert()
+
+    def open_update_goal_form(self, widget):
+        model, self.selected_row = self.notebook_window.goal_view.get_selection().get_selected()
+        if self.selected_row is not None:
+            self.entry_window = GoalEntryWindow()
+            self.entry_window.connect('save-updated', self.update_in_list_store)
+            self.entry_window.show_for_update(model[self.selected_row])
+
+    def delete_selected_goal_row(self, widget):
+        (model, pathlist) = self.notebook_window.goal_view.get_selection().get_selected_rows()
+        tree_iter = model.get_iter(pathlist[0])
+        id = model.get_value(tree_iter, 0)
+        self.model.delete_goal(id)
+
+    def open_insert_type_form(self, widget):
+        self.entry_window = TypeEntryWindow()
+        self.entry_window.connect('save-inserted', self.insert_in_list_store)
+        self.entry_window.show_for_insert()
+
+    def open_update_type_form(self, widget):
+        model, self.selected_row = self.notebook_window.type_view.get_selection().get_selected()
+        if self.selected_row is not None:
+            self.entry_window = TypeEntryWindow()
+            self.entry_window.connect('save-updated', self.update_in_list_store)
+            self.entry_window.show_for_update(model[self.selected_row])
+
+    def delete_selected_type_row(self, widget):
+        (model, pathlist) = self.notebook_window.type_view.get_selection().get_selected_rows()
+        tree_iter = model.get_iter(pathlist[0])
+        id = model.get_value(tree_iter, 0)
+        self.model.delete_type(id)
 
     def insert_in_list_store(self, widget):
         rows = self.entry_window.get_saving_fields()
         if isinstance(widget, OperationEntryWindow):
-            self.model.insert_into_operation(rows)
+            self.model.insert_operation(rows)
+        elif isinstance(widget, GoalEntryWindow):
+            self.model.insert_goal(rows)
+        elif isinstance(widget, TypeEntryWindow):
+            self.model.insert_type(rows)
 
     def update_in_list_store(self, widget):
         row = self.entry_window.get_saving_fields()
-        self.model.list_store[self.selected_row][0] = row[0]
-        self.model.list_store[self.selected_row][1] = row[1]
-        self.model.list_store[self.selected_row][2] = float(row[2])
+        if isinstance(widget, OperationEntryWindow):
+            (model, pathlist) = self.notebook_window.operation_view.get_selection().get_selected_rows()
+            tree_iter = model.get_iter(pathlist[0])
+            id = model.get_value(tree_iter, 0)
+            self.model.update_operation(row, id)
+        elif isinstance(widget, GoalEntryWindow):
+            (model, pathlist) = self.notebook_window.goal_view.get_selection().get_selected_rows()
+            tree_iter = model.get_iter(pathlist[0])
+            id = model.get_value(tree_iter, 0)
+            self.model.update_goal(row, id)
+        elif isinstance(widget, TypeEntryWindow):
+            (model, pathlist) = self.notebook_window.type_view.get_selection().get_selected_rows()
+            tree_iter = model.get_iter(pathlist[0])
+            id = model.get_value(tree_iter, 0)
+            self.model.update_type(row, id)
+
+
+
--- money_balance/model.py
+++ money_balance/model.py
@@ -1,38 +1,90 @@
 import sqlalchemy as sa
 from money_balance import Gtk
 
+
 class Model:
     def __init__(self):
         self.conn = sa.create_engine("postgresql://postgres:postgres@92.255.198.104:5432/money_balance")
 
-        self.operation_list_store = Gtk.ListStore(int, str, str, str)
+        self.operation_list_store = Gtk.ListStore(int, int, str, str, str)
         self.populate_operation(self.operation_list_store)
 
-
-        self.goal_list_store = Gtk.ListStore(int, str, str, str, bool, str)
-        self.populate_goal(self.operation_list_store)
+        self.goal_list_store = Gtk.ListStore(int, int, str, str, str, bool, str)
+        self.populate_goal(self.goal_list_store)
 
         self.type_list_store = Gtk.ListStore(str, int)
-        self.populate_type(self.operation_list_store)
-
+        self.populate_type(self.type_list_store)
 
     def populate_operation(self, list_store):
-        operation_rows = self.conn.execute('SELECT money, type, description, create_date FROM finance.operation')
+        list_store.clear()
+        operation_rows = self.conn.execute('SELECT operation_id, money, type, description, create_date '
+                                           'FROM finance.operation')
         for item in operation_rows:
-            self.operation_list_store.append([item[0], item[1], item[2], str(item[3])])
+            list_store.append([item[0], item[1], item[2], item[3], str(item[4])])
 
     def populate_goal(self, list_store):
-        goal_rows = self.conn.execute('SELECT money, type, description, priority, completed, create_date '
+        list_store.clear()
+        goal_rows = self.conn.execute('SELECT goal_id, money, type, description, priority, completed, create_date '
                                       'FROM finance.goal')
         for item in goal_rows:
-            self.goal_list_store.append([item[0], item[1], item[2], str(item[3]), item[4], str(item[5])])
+            list_store.append([item[0], item[1], item[2], item[3], str(item[4]), item[5], str(item[6])])
 
     def populate_type(self, list_store):
+        list_store.clear()
         type_rows = self.conn.execute('SELECT type, default_value FROM finance.type')
-
         for item in type_rows:
-            self.type_list_store.append([item[0], item[1]])
+            list_store.append([item[0], item[1]])
 
-    def insert_into_operation(self, row):
+    def insert_operation(self, row):
         self.conn.execute("INSERT INTO finance.operation (money, type, description) VALUES("
                           + row[0] + ",'" + row[1] + "','" + row[2] + "')")
+        self.populate_operation(self.operation_list_store)
+
+    def update_operation(self, row, id):
+        self.conn.execute("UPDATE finance.operation SET money = "
+                          + row[0] + " ,type = '"
+                          + row[1] + "' ,description = '"
+                          + row[2] + "' WHERE operation_id = " + str(id))
+        self.populate_operation(self.operation_list_store)
+
+    def delete_operation(self, id):
+        self.conn.execute('DELETE FROM finance.operation WHERE operation_id=' + str(id))
+        self.populate_operation(self.operation_list_store)
+
+    def insert_goal(self, row):
+        self.conn.execute("INSERT INTO finance.goal (money, type, description, priority, completed) VALUES("
+                          + row[0] + ",'" + row[1] + "','" + row[2] + "',"+ row[3] + "," + str(row[4]) + ")")
+        self.populate_goal(self.goal_list_store)
+
+    def update_goal(self, row, id):
+        self.conn.execute("UPDATE finance.goal SET money = " + row[0]
+                          + " ,type = '" + row[1]
+                          + "' ,description = '" + row[2]
+                          + "' ,priority = " + row[3]
+                          + ", completed = " + str(row[4])
+                          + " WHERE goal_id = " + str(id))
+        self.populate_goal(self.goal_list_store)
+
+    def delete_goal(self, id):
+        self.conn.execute('DELETE FROM finance.goal WHERE goal_id=' + str(id))
+        self.populate_goal(self.goal_list_store)
+
+    def insert_type(self, row):
+        if row[1] != '':
+            self.conn.execute("INSERT INTO finance.type (type, default_value) VALUES('"
+                          + row[0] + "'," + row[1] + ")")
+        else:
+            self.conn.execute("INSERT INTO finance.type (type) VALUES('"
+                              + row[0] + "')")
+        self.populate_type(self.type_list_store)
+
+    def update_type(self, row, type):
+        self.conn.execute("UPDATE finance.type SET type = '" + row[0]
+                          + "', default_value = " + row[1]
+                          + " WHERE type = '" + type + "'")
+        self.populate_type(self.type_list_store)
+
+    def delete_type(self, type):
+        self.conn.execute("DELETE FROM finance.type WHERE type='" + type + "'")
+        self.populate_type(self.type_list_store)
+
